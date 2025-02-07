e46da9fadbdd6eb55c574a7c60dadd875c0d6720
Euiseo Cha <zeroday0619_dev@outlook.com>
2025-02-07 15:47:59 +0900

    feat(tools): rust 프로그래밍 언어 대응

--- tools/create_py.py
+++ tools/create_py.py
@@ -1,27 +1,29 @@
 import os
 
+
 def create_python_script(problem_number: int):
     file_path = f"{problem_number}.py"
     if not os.path.exists(file_path):
         with open(file_path, "w") as file:
             file.write(
-                f"import sys\n\n"
-                f"# Your code here\n\n"
-                f"if __name__ == \"__main__\":\n"
-                f"    # Your code here\n"
-                f"    pass\n"
+                "import sys\n\n"
+                "# Your code here\n\n"
+                'if __name__ == "__main__":\n'
+                "    # Your code here\n"
+                "    pass\n"
             )
         print(f"Python script {file_path} has been created.")
     else:
         print(f"Python script {file_path} already exists.")
+
 
 def create_c_script(problem_number: int):
     file_path = f"{problem_number}.c"
     if not os.path.exists(file_path):
         with open(file_path, "w") as file:
             file.write(
-                f"#include <stdio.h>\n\n"
-                f"int main()\n"
+                "#include <stdio.h>\n\n"
+                "int main()\n"
                 "{\n"
                 "    // Your code here\n"
                 "    return 0;\n"
@@ -31,17 +33,34 @@ def create_c_script(problem_number: int):
     else:
         print(f"C script {file_path} already exists.")
 
+
+def create_rust_script(problem_number: int):
+    file_path = f"{problem_number}.rs"
+    if not os.path.exists(file_path):
+        with open(file_path, "w") as file:
+            file.write(
+                "use std::io;\n\n" "fn main()\n" "{\n" "    // Your code here\n" "}\n"
+            )
+        print(f"Rust script {file_path} has been created.")
+    else:
+        print(f"Rust script {file_path} already exists.")
+
+
 if __name__ == "__main__":
     try:
         problem_number = input("Enter the problem number: ")
         selected_language = input("Choose a language (Python/C): ").lower()
 
-        if selected_language == "python":
-            create_python_script(int(problem_number))
-        elif selected_language == "c":
-            create_c_script(int(problem_number))
-        else:
-            print("Invalid language. Please choose either Python or C.")
+        match selected_language:
+            case "python":
+                create_python_script(int(problem_number))
+            case "c":
+                create_c_script(int(problem_number))
+            case "rust":
+                create_rust_script(int(problem_number))
+            case _:
+                print("Invalid language. Please choose either Python or C.")
+
     except KeyboardInterrupt:
         print("\nExiting...")
         exit(0)
